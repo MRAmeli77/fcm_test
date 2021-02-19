0c754314de308d7cfc0be24f1c25dc38ce113587
Reza ADB <adbrezaooo@gmail.com>
2021-02-19 14:27:20 +0330

    server_side->routes->web.php: subscribe and unsubscribe routes added.

--- server side/routes/web.php
+++ server side/routes/web.php
@@ -18,56 +18,91 @@ use Kreait\Firebase\Messaging\Notification;
 |
 */
 
-Route::get('/', function () {
-    $deviceToken = "csP8ErItRL-VbfPrJ1JCno:APA91bFHFpnCy6XSloDuMZFhL-0PlPaSOy4nNVHYmoOz-F24IE7ErEFPLPWHIj6Sd5aU5LLcyVgx4YxN0787Ww7x8UbEYo2TD1v6RWgqnMB-xtxJrZ3bHnA6cvs9C6qDsDXUMIFgqY6j";
+Route::get('/SendNotification', function () {
     $factory = (new Factory)->withServiceAccount(__DIR__ . '\..\fcmtest-73a06-firebase-adminsdk-nafl1-ae84c5f43d.json');
-
     $messaging = $factory->createMessaging();
 
-    $message = CloudMessage::withTarget('token', $deviceToken)->withNotification(Notification::create('Title is title', 'Body is body2'))
-    ->withData(['title' => 'text messsssage']);
-    $messaging->send($message);
+//    $topic = 'a-topic';
+//    $message = CloudMessage::withTarget('topic', $topic)
+//        ->withNotification($notification) // optional
+//        ->withData($data) // optional
+//    ;
 
-    //    $url = 'https://fcm.googleapis.com/fcm/send';
-//    $fields = array(
-//        'registration_ids' => array(
-//            $id
-//        ),
-//        'data' => array(
-//            "message" => $message
-//        )
-//    );
-//    $fields = json_encode($fields);
-//    $headers = array(
-//        'Authorization: key=' . "AAAANj7oQjM:APA91bF1AtV0sRajn3SsVNalTrjBlTFciejvJ8nZMtygAu7IBjUzsev8ovFi1687BmRXjRXUcgevjQ2G5k2I_QOeI7EoQWqWe2kHw7lXKz4tdpHG9gmlbBDdJeJdwl2aun7kmgFGc3L7",
-//        'Content-Type: application/json'
-//    );
-//    $ch = curl_init();
-//    curl_setopt($ch, CURLOPT_URL, $url);
-//    curl_setopt($ch, CURLOPT_POST, true);
-//    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-//    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//    curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
-//    $result = curl_exec($ch);
-//    echo $result;
-//    curl_close($ch);
+//    $condition = "'TopicA' in topics && ('TopicB' in topics || 'TopicC' in topics)";
+///*
+//    OR-conditions are currently not processed correctly by the Firebase Rest API, leading to undelivered messages.
+//    This can be resolved by splitting up a message to an OR-condition into multiple messages to AND-conditions.
+//    So one conditional message to 'a' in topics || 'b' in topics should be sent as
+//    two messages to the conditions 'a' in topics && !('b' in topics) and 'b' in topics && !('a' in topics)
+//    References:
+//        https://github.com/firebase/quickstart-js/issues/183
+//        https://stackoverflow.com/a/52302136/284325
+//*/
+//    $message = CloudMessage::withTarget('condition', $condition)
+//        ->withNotification($notification) // optional
+//        ->withData($data) // optional
+//    ;
+
+    $data = ['title' => 'text messsssage'];
+    $deviceToken = "add token here";
+    $message = CloudMessage::withTarget('token', $deviceToken)->withNotification(Notification::create('Title is title', 'Body is body'))->withData($data);
+    try {
+        $messaging->send($message);
+    } catch (Exception $e) {
+        var_dump($e->getMessage());
+    }
 });
-//Route::get('/tokenCheck/', function () {
-//    $deviceToken = "fQ9GlswcQDeoVzBkSgF0S5:APA91bHBMZBSO8lifdlpwmFLjE1Z7f1XxBHp0k5rVRvrek51pmgDjOfpXAxTcLMqPWv-0xn7gAV6Dj6NRq1ifhEe7ATw5uRogQ-qpqtXYGcsx-xmKSL1qNbWH3p4Ndf-woi46KbvoYVG";
-//    $deviceToken = "fQ9GlswcQDeoVzBkSgF0S5:APA91bHBMZBSO8lifdlpwmFLjE1Z7f1XxBHp0k5rVRvrek51pmgDjOfpXAxTcLMqPWv-0xn7gAV6Dj6NRq1ifhEe7ATw5uRogQ-qpqtXYGcsx-xmKSL1qNbWH3p4Ndf-woi46KbvoYVG";
-//    $factory = (new Factory)->withServiceAccount(__DIR__.'\fcmtest-73a06-firebase-adminsdk-nafl1-ae84c5f43d.json');
-//    $messaging = $factory->createMessaging();
+Route::get('/SendNotification/{deviceToken}', function ($deviceToken) {
+    $factory = (new Factory)->withServiceAccount(__DIR__ . '\..\fcmtest-73a06-firebase-adminsdk-nafl1-ae84c5f43d.json');
+    $messaging = $factory->createMessaging();
+    $data = ['title' => 'text messsssage'];
+    $message = CloudMessage::withTarget('token', $deviceToken)->withNotification(Notification::create('Title is title', 'Body is body'))
+        ->withData($data);
+    try {
+        $messaging->send($message);
+    } catch (Exception $e) {
+        var_dump($e->getMessage());
+    }
+});
+Route::get('/tokenCheck/{deviceToken}', function ($deviceToken) {
+    $factory = (new Factory)->withServiceAccount(__DIR__ . '\fcmtest-73a06-firebase-adminsdk-nafl1-ae84c5f43d.json');
+    $messaging = $factory->createMessaging();
 //    $message = CloudMessage::withTarget('token', $deviceToken);
-//    $message = CloudMessage::fromArray([
-//        'token' => $deviceToken,
-//        'notification' => Notification::create('Title is title', 'Body is not body') // optional
-//    ]);
-//
-//    try {
-//        $response = $messaging->send($message);
-//        var_dump($response);
-//    } catch (Exception $e) {
-//        echo($e->getMessage());
-//    }
-//
-//});
+    $message = CloudMessage::fromArray([
+        'token' => $deviceToken,
+        'notification' => Notification::create('Title is title', 'Body is not body') // optional
+    ]);
+    try {
+        $response = $messaging->send($message);
+        var_dump($response);
+    } catch (Exception $e) {
+        var_dump($e->getMessage());
+    }
+});
+Route::get('/subscribeUser/{deviceToken}/topic/{topic}', function ($deviceToken, $topic) {
+    /*
+     * You can subscribe client app instances to any existing topic, or you can create a new topic.
+     * When you use the API to subscribe a client app to a new topic (one that does not already exist for your Firebase project),
+     * a new topic of that name is created in FCM and any client can subsequently subscribe to it.
+     */
+
+    $factory = (new Factory)->withServiceAccount(__DIR__ . '\fcmtest-73a06-firebase-adminsdk-nafl1-ae84c5f43d.json');
+    $messaging = $factory->createMessaging();
+    $result = $messaging->subscribeToTopic($topic, $deviceToken);
+//    $result = $messaging->subscribeToTopics($topics, $registrationTokenOrTokens);
+    var_dump($result);
+});
+Route::get('/unsubscribeUser/{deviceToken}', function ($deviceToken) {
+    $factory = (new Factory)->withServiceAccount(__DIR__ . '\fcmtest-73a06-firebase-adminsdk-nafl1-ae84c5f43d.json');
+    $messaging = $factory->createMessaging();
+    $result = $messaging->unsubscribeFromAllTopics($deviceToken);
+    var_dump($result);
+});
+
+Route::get('/unsubscribeUser/{deviceToken}/topic/{topic}', function ($deviceToken, $topic) {
+    $factory = (new Factory)->withServiceAccount(__DIR__ . '\fcmtest-73a06-firebase-adminsdk-nafl1-ae84c5f43d.json');
+    $messaging = $factory->createMessaging();
+    $result = $messaging->unsubscribeFromTopic($topic, $deviceToken);
+//    $result = $messaging->unsubscribeFromTopics($topics, $registrationTokenOrTokens);
+    var_dump($result);
+});
